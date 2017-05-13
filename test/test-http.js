b2d3e525e4194a665592c5bac21f0de5fd70c71b
sretundijr <sretundijr@gmail.com>
2017-05-12 20:56:01 -0600

    get test built and passed, building post

--- test/test-http.js
+++ test/test-http.js
@@ -1,6 +1,44 @@
 
-
-const chai = require('chai').should();
+const chai = require('chai');
 const chaiHttp = require('chai-http');
 
+const { app, runServer, closeServer } = require('../server');
 
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Blog Api', function () {
+    before(function () {
+        return runServer();
+    });
+    after(function () {
+        return closeServer();
+    });
+
+    it('Should get all blog post', function () {
+        return chai.request(app)
+            .get('/blog-posts')
+            .then(function (res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.length.should.be.above(0);
+                res.body.forEach(function (item) {
+                    item.should.have.all.keys(
+                        'id', 'title', 'author', 'content', 'publishDate'
+                    );
+                })
+            })
+    })
+
+    it('Should post new blog posts', function () {
+        let createNewBlogPost = { title: 'HelloTest', content: 'testing blog posts', author: 'Steve-o' }
+        return chai.request(app)
+            .post('/blog-posts')
+            .send(createNewBlogPost)
+            .then(function (res) {
+                res.should.have.status(201);
+            })
+    })
+})
